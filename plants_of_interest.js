d098a99136b6ef802c10244a6192506feee638f6
urbana-home <150568454+urbana-home@users.noreply.github.com>
2023-12-05 16:05:15 -0600

    Update plants_of_interest.js

--- plants_of_interest.js
+++ plants_of_interest.js
@@ -1,75 +1,586 @@
 const plantsOfInterest = [
-"Polystichum arostichoides",
-"Anemone canadensis",
-"Asarum europaeum",
-"Amsonia hubrichtii",
-"Anemone x hybrida 'honorine Jobert'",
-"Artemisia 'Silver King'",
-"Aruncus doicus",
-"Asciepias purpurascans",
-"Actea racemose",
-"Asciepia tuberosa",
-"Baptisia australis",
-"Baptista alba",
-"Berginia 'Winter Glow'",
-"Coreopsis plamata",
-"Coreopsis lanceolata",
-"Callirhoe involucrata",
-"Campanula rotundifolia",
-"Convallaria majaiis",
-"Chelone 'Hotlips'",
-"Dianthus carthusiana",
-"Dicentra spectibilis",
-"Eryngium 'Blue Star'",
-"Echinops ritro",
-"Eurybia divaricatus",
-"Echinacea 'Harvest Moon'",
-"Euphorbia corollata",
-"Echinacea 'Tiki Torch'",
-"Echinacea pallida",
-"Eupatorium purpureum",
-"Echinacca 'Pixie Meadowbrite'",
-"Gemium Spessart",
-"Gemaium sanguineum Max Frei",
-"Gillenia trifoliata",
-"Heliopsis 'Sommersonne'",
-"Helleborus foctidus",
-"Helleborus s hybridus",
-"Hosta plantaginca clegans",
-"Helianthus 'Lemon Oucen'",
-"Helianthus mollis",
-"Liatris aspera",
-"Lychnis chaledonica",
-"Liriope spicata",
-"Origanum vulgare",
-"Persicaria 'Firetail'",
-"Phlos pilosa",
-"Petalostemum purpurcum",
-"Solidago cacsia",
-"Symphoricarpus orbiculata",
-"Symphytotrichum cordifolium",
-"Stachys 'Hummelo'",
-"Spirca tonentosa",
-"Solidago juncea",
-"Sanguisorba menziesii",
-"Symphytotrichum oolentangiense",
-"Siliphium terebinthinaceum",
-"Scabiosa culmaria 'Butterfly Blue'",
-"Round leaf ragwort",
-"Thalictrum dasycarpum",
-"Tellima grandiflora",
-"Verbena bonariensis",
-"Vinca minor 'Bowles'",
-"Verbascum nigrum 'Album'",
-"Xanthorhyza simplicissima",
-"Bouteloua curtipendula",
-"Carex albicans",
-"Carex eburnea",
-"Carex muskingumensis",
-"Carex pennsylvanica",
-"Eragrostis spectibilis",
-"Hystrix patula",
-"Pennisetum 'Karley Rose'",
-"Sporobolis heterolepsis",
-"Schizachyrium scoparium 'Jazz'"
+	{
+		'name': "Polystichum arostichoides",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Anemone canadensis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Asarum europaeum",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Amsonia hubrichtii",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Anemone x hybrida 'honorine Jobert'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Artemisia 'Silver King'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Aruncus doicus",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Asciepias purpurascans",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Actea racemose",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Asciepia tuberosa",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Baptisia australis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Baptista alba",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Berginia 'Winter Glow'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Coreopsis plamata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Coreopsis lanceolata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Callirhoe involucrata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Campanula rotundifolia",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Convallaria majaiis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Chelone 'Hotlips'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Dianthus carthusiana",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Dicentra spectibilis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Eryngium 'Blue Star'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Echinops ritro",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Eurybia divaricatus",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Echinacea 'Harvest Moon'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Euphorbia corollata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Echinacea 'Tiki Torch'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Echinacea pallida",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Eupatorium purpureum",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Echinacea 'Pixie Meadowbrite'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Geranium Spessart",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Geranium sanguineum Max Frei",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Gillenia trifoliata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Heliopsis 'Sommersonne'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Helleborus foetidus",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Helleborus x hybridus",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Hosta plantaginea elegans",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Helianthus 'Lemon Queen'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Helianthus mollis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Liatris aspera",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Lychnis chaledonica",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Liriope spicata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Origanum vulgare",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Persicaria 'Firetail'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Phlox pilosa",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Petalostemum purpureum",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Solidago caesia",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Symphoricarpus orbiculata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Symphytotrichum cordifolium",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Stachys 'Hummelo'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Spirea tormentosa",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Solidago juncea",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Sanguisorba menziesii",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Symphytotrichum oolentangiense",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Siliphium terebinthinaceum",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Scabiosa culmaria 'Butterfly Blue'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Packera obovata",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Thalictrum dasycarpum",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Tellima grandiflora",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Verbena bonariensis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Vinca minor 'Bowles'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Verbascum nigrum 'Album'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Xanthorhyza simplicissima",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Bouteloua curtipendula",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Carex albicans",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Carex eburnea",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Carex muskingumensis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Carex pennsylvanica",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Eragrostis spectibilis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Hystrix patula",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Pennisetum 'Karley Rose'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Sporobolis heterolepsis",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	},
+	{
+		'name': "Schizachyrium scoparium 'Jazz'",
+ 		'flower': '', 
+ 		'bloom': '', 
+ 		'height': '', 
+ 		'width': '', 
+ 		'preference': '' 
+ 	}
 ] 
