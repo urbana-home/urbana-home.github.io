fb641a7d6ea4c00e4247636c595fd713cc42b908
urbana-home <150568454+urbana-home@users.noreply.github.com>
2023-12-04 20:37:01 -0600

    Create plants_of_interest.js

---
+++ plants_of_interest.js
@@ -0,0 +1,86 @@
+// plantsOfInterest = [
+//   {
+//     'name': 'Amsonia hubrichtii',
+//     'height': 3,
+//     'width': 3,
+//     'foliage': 'green',
+//     'flower'
+//   }
+// ]
+
+
+plantsOfInterest = [
+"Polystichum arostichoides",
+"Anemone canadensis",
+"Asarum europaeum",
+"Amsonia hubrichtii",
+"Anemone x hybrida 'honorine Jobert'",
+"Artemisia 'Silver King'",
+"Aruncus doicus",
+"Asciepias purpurascans",
+"Actea racemose",
+"Asciepia tuberosa",
+"Baptisia australis",
+"Baptista alba",
+"Berginia 'Winter Glow'",
+"Coreopsis plamata",
+"Coreopsis lanceolata",
+"Callirhoe involucrata",
+"Campanula rotundifolia",
+"Convallaria majaiis",
+"Chelone 'Hotlips'",
+"Dianthus carthusiana",
+"Dicentra spectibilis",
+"Eryngium 'Blue Star'",
+"Echinops ritro",
+"Eurybia divaricatus",
+"Echinacea 'Harvest Moon'",
+"Euphorbia corollata",
+"Echinacea 'Tiki Torch'",
+"Echinacea pallida",
+"Eupatorium purpureum"
+] 
+// Echinacca 'Pixie Meadowbrite*
+// Gemium Spessart'
+// Gemaium sanguineum Max Frei
+// Gillenia trifoliata
+// Heliopsis 'Sommersonne'
+// Helleborus foctidus
+// Helleborus s hybridus
+// Hosta plantaginca clegans
+// Helianthus 'Lemon Oucen®
+// Helianthus mollis
+// Liatris aspera
+// Lychnis chaledonica
+// Liriope spicata
+// Origanum vulgare
+// Persicaria 'Firetail*
+// Phlos pilosa
+// Petalostemum purpurcum
+// Solidago cacsia
+// Symphoricarpus orbiculata
+// Symphytotrichum cordifolium
+// Stachys 'Hummelo'
+// Spirca tonentosa
+// Solidago juncea
+// Sanguisorba menziesii
+// Symphytotrichum oolentangiense
+// Siliphium terebinthinaceum
+// Scabiosa culmaria 'Butterfly Blue'
+// Round leaf ragwort
+// Thalictrum dasycarpum
+// Tellima grandiflora
+// Verbena bonariensis
+// Vinca minor 'Bowles*
+// Verbascum nigrum 'Album'
+// Xanthorhyza simplicissima
+// Bouteloua curtipendula
+// Carex albicans
+// Carex eburnea
+// Carex muskingumensis
+// Carex pennsylvanica
+// Eragrostis spectibilis
+// Hystrix patula
+// Pennisetum 'Karley Rose'
+// Sporobolis heterolepsis
+// Schizachyrium scoparium 'Jazz'
