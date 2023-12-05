300e1a0532237a0e51cb5fd3287185567ad68ad3
urbana-home <150568454+urbana-home@users.noreply.github.com>
2023-12-04 20:57:10 -0600

    Update plants_of_interest.js

--- plants_of_interest.js
+++ plants_of_interest.js
@@ -1,14 +1,3 @@
-// plantsOfInterest = [
-//   {
-//     'name': 'Amsonia hubrichtii',
-//     'height': 3,
-//     'width': 3,
-//     'foliage': 'green',
-//     'flower'
-//   }
-// ]
-
-
 plantsOfInterest = [
 "Polystichum arostichoides",
 "Anemone canadensis",
@@ -38,49 +27,51 @@ plantsOfInterest = [
 "Euphorbia corollata",
 "Echinacea 'Tiki Torch'",
 "Echinacea pallida",
-"Eupatorium purpureum"
-] 
-// Echinacca 'Pixie Meadowbrite*
-// Gemium Spessart'
-// Gemaium sanguineum Max Frei
-// Gillenia trifoliata
-// Heliopsis 'Sommersonne'
-// Helleborus foctidus
-// Helleborus s hybridus
-// Hosta plantaginca clegans
-// Helianthus 'Lemon Oucen®
-// Helianthus mollis
-// Liatris aspera
-// Lychnis chaledonica
-// Liriope spicata
-// Origanum vulgare
-// Persicaria 'Firetail*
-// Phlos pilosa
-// Petalostemum purpurcum
-// Solidago cacsia
-// Symphoricarpus orbiculata
-// Symphytotrichum cordifolium
-// Stachys 'Hummelo'
-// Spirca tonentosa
-// Solidago juncea
-// Sanguisorba menziesii
-// Symphytotrichum oolentangiense
-// Siliphium terebinthinaceum
-// Scabiosa culmaria 'Butterfly Blue'
-// Round leaf ragwort
-// Thalictrum dasycarpum
-// Tellima grandiflora
-// Verbena bonariensis
-// Vinca minor 'Bowles*
-// Verbascum nigrum 'Album'
-// Xanthorhyza simplicissima
-// Bouteloua curtipendula
-// Carex albicans
-// Carex eburnea
-// Carex muskingumensis
-// Carex pennsylvanica
-// Eragrostis spectibilis
-// Hystrix patula
-// Pennisetum 'Karley Rose'
-// Sporobolis heterolepsis
-// Schizachyrium scoparium 'Jazz'
+"Eupatorium purpureum",
+"Echinacca 'Pixie Meadowbrite'",
+"Gemium Spessart",
+"Gemaium sanguineum Max Frei",
+"Gillenia trifoliata",
+"Heliopsis 'Sommersonne'",
+"Helleborus foctidus",
+"Helleborus s hybridus",
+"Hosta plantaginca clegans",
+"Helianthus 'Lemon Oucen'",
+"Helianthus mollis",
+"Liatris aspera",
+"Lychnis chaledonica",
+"Liriope spicata",
+"Origanum vulgare",
+"Persicaria 'Firetail'",
+"Phlos pilosa",
+"Petalostemum purpurcum",
+"Solidago cacsia",
+"Symphoricarpus orbiculata",
+"Symphytotrichum cordifolium",
+"Stachys 'Hummelo'",
+"Spirca tonentosa",
+"Solidago juncea",
+"Sanguisorba menziesii",
+"Symphytotrichum oolentangiense",
+"Siliphium terebinthinaceum",
+"Scabiosa culmaria 'Butterfly Blue'",
+"Round leaf ragwort",
+"Thalictrum dasycarpum",
+"Tellima grandiflora",
+"Verbena bonariensis",
+"Vinca minor 'Bowles'",
+"Verbascum nigrum 'Album'",
+"Xanthorhyza simplicissima",
+"Bouteloua curtipendula",
+"Carex albicans",
+"Carex eburnea",
+"Carex muskingumensis",
+"Carex pennsylvanica",
+"Eragrostis spectibilis",
+"Hystrix patula",
+"Pennisetum 'Karley Rose'",
+"Sporobolis heterolepsis",
+"Schizachyrium scoparium 'Jazz'"
+]  
+
+export { plantsOfInterest };
